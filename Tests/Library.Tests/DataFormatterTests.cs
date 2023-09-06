3d13ba6ff710ddbbb95f96d7ed9ab329556431bb
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-06 10:12:48 -0300

    Agrergo tests

---
+++ Tests/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,34 @@
+
+namespace TestDateFormat;
+
+public class DataFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+
+    public void FechaCorrecta()
+    {
+        string input = "20/09/2002";
+        string expected = "2002-09-20";
+        string output = DateFormatter.ChangeFormat(input);
+        Assert.AreEqual(output, expected);
+    }
+    public void SrtingVacio()
+    {
+        string input = "";
+        string expected = "2002-09-20";
+        string output = DateFormatter.ChangeFormat(input);
+        Assert.AreEqual(output, expected);
+    }
+    public void Invertida()
+    {
+        string input = "20/09/2002";
+        string expected = "2002-09-20";
+        string output = DateFormatter.ChangeFormat(input);
+        Assert.AreEqual(output, expected);
+    }
+}
